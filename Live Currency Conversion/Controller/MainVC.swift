4393c926bc74cb8dc5f077d277ec4112aa9b8cc2
Khaled Elfakharany <fakharany.developer@gmail.com>
2019-10-10 02:19:29 +0300

    move cell in table view

--- Live Currency Conversion/Controller/MainVC.swift
+++ Live Currency Conversion/Controller/MainVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var currencyTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +20,22 @@ class MainVC: UIViewController {
 
 }
 
+extension MainVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .red
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: cell.layer.frame.width, height: cell.layer.frame.height))
+        label.text = "\(indexPath.row)"
+        cell.addSubview(label)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let ind = IndexPath(row: 0, section: 0)
+        currencyTable.moveRow(at: indexPath, to: ind)
+    }
+}
