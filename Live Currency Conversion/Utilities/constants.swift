c207c31dc4168c96077e4f61f27115d82816b63b
Khaled Elfakharany <fakharany.developer@gmail.com>
2019-10-10 04:48:34 +0300

    Keyboard

---
+++ Live Currency Conversion/Utilities/constants.swift
@@ -0,0 +1,9 @@
+//
+//  constants.swift
+//  Live Currency Conversion
+//
+//  Created by Khaled Fakharany on 10/10/19.
+//  Copyright © 2019 Khaled Fakharany. All rights reserved.
+//
+
+import Foundation
