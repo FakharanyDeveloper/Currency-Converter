59396925e381321ec17bdc15b188f687a298e0bd
Khaled Elfakharany <fakharany.developer@gmail.com>
2019-10-10 23:24:00 +0300

    Only EUR as base currency

--- Live Currency Conversion/Controller/MainVC.swift
+++ Live Currency Conversion/Controller/MainVC.swift
@@ -33,7 +33,9 @@ class MainVC: UIViewController {
                 }
             }
             self.currancies = self.currancies.sorted(by: { $0.code < $1.code })
-            self.currancies.insert(Currency(code: self.baseCur, value: 1), at: 0)
+            if self.baseCur == "EUR" {
+                self.currancies.insert(Currency(code: self.baseCur, value: 1), at: 0)
+            }
             self.currencyTable.reloadData()
         }
     }
